3513688345e4ec00856860c549d0e927e73bec49
iversonli <iversonli88@gmail.com>
2025-04-11 20:20:11 -0700

    Implementing Money Class.

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -8,7 +8,69 @@ struct DomainModel {
 ////////////////////////////////////
 // Money
 //
+
+import Foundation
+
 public struct Money {
+    
+    var amount: Int
+    var currency: String
+    
+    enum MoneyError: Error {
+        case invalidCurrency(String)
+    }
+    
+    init(amount: Int, currency: String) {
+        self.amount = amount
+        self.currency = currency.uppercased()
+    }
+    
+    func convert(_ targetCurrency: String) -> Money {
+
+        let amountInUSD: Double
+        switch currency {
+        case "USD":
+            amountInUSD = Double(amount)
+        case "GBP":
+            amountInUSD = Double(amount) * 2.0
+        case "EUR":
+            amountInUSD = Double(amount) / 1.5
+        case "CAN":
+            amountInUSD = Double(amount) / 1.25
+        default:
+            print("invalid currency: \(currency)")
+            return self
+        }
+        
+        let targetAmount: Int
+        switch targetCurrency {
+        case "USD":
+            targetAmount = Int(amountInUSD.rounded())
+        case "GBP":
+            targetAmount = Int((amountInUSD * 0.5).rounded())
+        case "EUR":
+            targetAmount = Int((amountInUSD * 1.5).rounded())
+        case "CAN":
+            targetAmount = Int((amountInUSD * 1.25).rounded())
+        default:
+            print("invalid currency: \(targetCurrency)")
+            return self
+        }
+
+        return Money(amount: targetAmount, currency: targetCurrency)
+    }
+    
+    func add(_ args: Money) -> Money {
+        let converted = args.convert(currency)
+        let sum = amount + converted.amount
+        return Money(amount: sum, currency: currency)
+    }
+
+    func subtract(_ args: Money) -> Money {
+        let converted = args.convert(currency)
+        let difference = amount - converted.amount
+        return Money(amount: difference, currency: currency)
+    }
 }
 
 ////////////////////////////////////
